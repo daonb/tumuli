5b66ab7daa7cf24ecd710001cc84d358208b4501
Benny Daon <bennydaon@gmail.com>
2018-01-28 13:23:37 +0200

    simplifying interface by adding inlines - resolves #11

--- biography/admin.py
+++ biography/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
-from biography.models import Biography, Season, ContentAtom, Memoir
 
-# Register your models here.
-admin.site.register(Biography)
-admin.site.register(Season)
+from .models import Biography, Season, ContentAtom, Memoir
+
+
+class SeassonInline(admin.StackedInline):
+    model = Season
+
+
+class BiographyAdmin(admin.ModelAdmin):
+    model = Biography
+    inlines = [
+        SeassonInline,
+    ]
+
+
+class MemoirInline(admin.StackedInline):
+    model = Memoir
+
+
+class SeasonAdmin(admin.ModelAdmin):
+    model = Season
+    inlines = [
+        MemoirInline,
+    ]
+
+
+admin.site.register(Biography, BiographyAdmin)
+admin.site.register(Season, SeasonAdmin)
 admin.site.register(Memoir)
 admin.site.register(ContentAtom)
