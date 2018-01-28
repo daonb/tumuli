6c2e618e82bbba8e16402713a05c7b376713f1d3
benny daon <bennydaon@gmail.com>
2018-01-28 21:51:56 +0200

    adding s3 storage - #16

--- tumuli/settings.py
+++ tumuli/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'storages',
     'biography',
 ]
 
@@ -137,3 +138,13 @@ STATICFILES_DIRS = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+if 'AWS_SECRET_ACCESS_KEY' in os.environ:
+    # S3 media
+    AWS_DEFAULT_ACL="public-read"
+    AWS_STORAGE_BUCKET_NAME=os.environ['MEDIA_S3_BUCKET']
+    AWS_S3_SIGNATURE_VERSION="s3v4"
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_SECRET_ACCESS_KEY= os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_ACCESS_KEY_ID=os.environ['AWS_ACCESS_KEY_ID']
+    AWS_S3_REGION_NAME=os.environ['AWS_REGION_NAME']
