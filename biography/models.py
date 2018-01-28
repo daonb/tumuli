27d3686314d17299a64cac45669f773222a00925
benny daon <bennydaon@gmail.com>
2018-01-28 18:22:45 +0200

    Now... in Hebrew! - resolves #14

--- biography/models.py
+++ biography/models.py
@@ -2,21 +2,30 @@ import uuid
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class Memoir(models.Model):
     ''' a story connection a memory dump connected to a person's season '''
     author = models.ForeignKey(
-        User, null=True, on_delete=models.CASCADE, related_name='memoirs')
-    season = models.ForeignKey('Season', on_delete=models.CASCADE)
-    content = models.ForeignKey('ContentAtom', null=True, blank=True,
-                                on_delete=models.SET_NULL)
-
-    story_audio = models.FileField(null=True, blank=True)
+        User, verbose_name=_("Author"), null=True,
+        on_delete=models.CASCADE, related_name='memoirs')
+    season = models.ForeignKey(
+        'Season', verbose_name=_("Season"), on_delete=models.CASCADE)
+    content = models.ForeignKey(
+        'ContentAtom', verbose_name=_("Content"),
+        null=True, blank=True, on_delete=models.SET_NULL)
+    story_audio = models.FileField(
+        null=True, verbose_name=_("Story Audio"), blank=True)
     story_text = models.TextField(
-        blank=True, help_text="context and description of the dump")
+        _("Story Text"), blank=True,
+        help_text="context and description of the dump")
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("Memoir")
+        verbose_name_plural = _("Memoirs")
 
     def get_auto_memoir(self):
         return self.objects.get(author__is_null=True)
@@ -24,12 +33,17 @@ class Memoir(models.Model):
 
 class Season(models.Model):
     ''' a meaningful period in a life '''
-    story_audio = models.FileField(null=True, blank=True)
-    story_text = models.TextField(blank=True)
-    place = models.TextField()
-    start_date = models.DateField()
-    end_date = models.DateField()
-    biography = models.ForeignKey('Biography', on_delete=models.CASCADE)
+    story_audio = models.FileField(_("Story Audio"), null=True, blank=True)
+    story_text = models.TextField(_("Story Text"), blank=True)
+    place = models.TextField(_("Place"))
+    start_date = models.DateField(_("Start Date"))
+    end_date = models.DateField(_("End Date"))
+    biography = models.ForeignKey(
+        'Biography', verbose_name=_("Biography"), on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("Season")
+        verbose_name_plural = _("Seasons")
 
 
 class Biography(models.Model):
@@ -37,21 +51,30 @@ class Biography(models.Model):
     uuid = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False)
     # TODO: dates should be fuzzy
-    date_of_birth = models.DateTimeField(blank=True, null=True)
-    date_of_passing = models.DateTimeField(blank=True, null=True)
-    place_of_birth = models.TextField()
-    place_of_passing = models.TextField(blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE,
-               help_text='all other personal data is stored on the user')
-    editors = models.ManyToManyField(User, related_name='editing_bios',
-                  help_text='the good people that can edit this biography')
+    date_of_birth = models.DateTimeField(
+        _("Date of birth"), blank=True, null=True)
+    date_of_passing = models.DateTimeField(
+        _("Date of passing"), blank=True, null=True)
+    place_of_birth = models.TextField(_("Place of birth"))
+    place_of_passing = models.TextField(
+        _("Place of passing"), blank=True, null=True)
+    user = models.ForeignKey(
+        User, verbose_name=_("Person"), on_delete=models.CASCADE,
+        help_text='all other personal data is stored on the user')
+    editors = models.ManyToManyField(User, verbose_name=_("Editors"),
+        related_name='editing_bios',
+        help_text='the good people that can edit this biography')
     # creator is critical when we have issues of ownership and liability
     creator = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='created_bios')
-    is_public = models.BooleanField(default=False,
+    is_public = models.BooleanField(_("Public"), default=False,
                     help_text='Is it public?')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("Biography")
+        verbose_name_plural = _("Biographies")
 
     @property
     def is_dead(self):
@@ -72,11 +95,18 @@ class Biography(models.Model):
 
 class ContentAtom(models.Model):
     ''' content atom '''
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    placed = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        User, verbose_name=_("Owner"), on_delete=models.CASCADE)
+    placed = models.DateTimeField(_("Import date"), auto_now_add=True)
     seasons = models.ManyToManyField(
         Season, through='Memoir', related_name='memoirs')
-    who = models.TextField(blank=True, help_text="who is in the atom?")
+    who = models.TextField(
+        _("Who"), blank=True, help_text="who is in the atom?")
     date = models.DateTimeField(
+        _("Original Date"),
         blank=True, null=True, help_text="When was this atom made?")
-    image = models.ImageField()
+    image = models.ImageField(verbose_name=_("Image"))
+
+    class Meta:
+        verbose_name = _("Content Atom")
+        verbose_name_plural = _("Content Atoms")
--- tumuli/settings.py
+++ tumuli/settings.py
@@ -103,7 +103,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'he'
+
+LANGUAGES = (
+        ('en-us', 'English'),
+        ('he', 'Hebrew'),
+)
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
