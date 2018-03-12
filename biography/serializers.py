84f952063dbe531a664b395a30fee8f680d5b9ba
Libi Gelber <gelber.libi@gmail.com>
2018-03-12 12:02:32 +0200

    Add a RESTfull interface, fixes #31

---
+++ biography/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from biography.models import Memoir, Period, Biography, ContentAtom
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name')
+
+
+class MemoirSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Memoir
+        fields = ('owner', 'author', 'period', 'content', 'story_audio',
+                  'story_text', 'notes', 'created', 'modified')
+
+
+class PeriodSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Period
+        fields = ('name', 'story_audio', 'notes', 'place',
+                  'start_date', 'end_date', 'biography')
+
+
+class BiographySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Biography
+        fields = ('uuid', 'date_of_birth', 'date_of_passing',
+                  'place_of_birth', 'place_of_passing', 'user',
+                  'editors', 'creator', 'is_public', 'created', 'modified')
+
+
+class ContentAtomSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ContentAtom
+        fields = ('owner', 'placed', 'periods',
+                  'who', 'where', 'date', 'image')
